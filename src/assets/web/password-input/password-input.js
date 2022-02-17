0ac5b71490fe88a04d5120932609b564508053f2
Elias Luhr <e.luhr@herzogkommunikation.de>
2022-02-17 19:41:49 +0100

    updated js

--- src/assets/web/password-input/password-input.js
+++ src/assets/web/password-input/password-input.js
@@ -1,51 +1,51 @@
 $.fn.passwordInput = function (options) {
-    options = options || {};
-    var _input = $(this);
-    var _widget = _input.closest('.password-input-widget');
-    var _button = _widget.find('.password-input-toggle-button');
-    var _ruleBars = _widget.find('.password-rule-bars');
-    var _ruleTexts = _widget.find('.password-rule-texts');
+    options = options || {}
+    var _input = $(this)
+    var _widget = _input.closest('.password-input-widget')
+    var _button = _widget.find('.password-input-toggle-button')
+    var _ruleBars = _widget.find('.password-rule-bars')
+    var _ruleTexts = _widget.find('.password-rule-texts')
     var _rules = options.rules || []
 
     _input.on("input", function () {
-        var inputValue = $(this).val();
+        var inputValue = $(this).val()
         for (var ruleIndex = 0; ruleIndex < _rules.length; ruleIndex++) {
             checkBar(ruleIndex, inputValue)
             checkText(ruleIndex, inputValue)
         }
-    });
+    })
 
     function checkBar(ruleIndex, inputValue) {
         var rule = _rules[ruleIndex]
-        var ruleBar = _ruleBars.find('.password-rule-bar[data-rule-index="' + ruleIndex + '"]');
+        var ruleBar = _ruleBars.find('.password-rule-bar[data-rule-index="' + ruleIndex + '"]')
         var isValid = patternMatches(inputValue, rule.pattern)
         if (isValid) {
-            ruleBar.addClass('matches');
+            ruleBar.addClass('matches')
         } else {
-            ruleBar.removeClass('matches');
+            ruleBar.removeClass('matches')
         }
     }
 
     function checkText(ruleIndex, inputValue) {
         var rule = _rules[ruleIndex]
-        var ruleText = _ruleTexts.find('.password-rule-text[data-rule-index="' + ruleIndex + '"]');
+        var ruleText = _ruleTexts.find('.password-rule-text[data-rule-index="' + ruleIndex + '"]')
         var isValid = patternMatches(inputValue, rule.pattern)
         if (isValid) {
-            ruleText.addClass('matches');
+            ruleText.addClass('matches')
         } else {
-            ruleText.removeClass('matches');
+            ruleText.removeClass('matches')
         }
     }
 
     function patternMatches(text, pattern) {
-        var regEx = new RegExp(pattern.replaceAll('\/', ''));
+        var regEx = new RegExp(pattern.replaceAll('\/', ''))
         return text.match(regEx) !== null
     }
 
     _button.on("click", function () {
-        var button = $(this);
-        var oldVisibility = button.data("password-visible");
-        var newVisibility = oldVisibility ? 0 : 1;
+        var button = $(this)
+        var oldVisibility = button.data("password-visible")
+        var newVisibility = oldVisibility ? 0 : 1
         button.data("password-visible", newVisibility)
 
         if (newVisibility === 1) {
@@ -55,5 +55,9 @@ $.fn.passwordInput = function (options) {
             button.button('hidden')
             _input.attr('type', 'password')
         }
+        _input.trigger('toggleVisibility', {
+            newVisibility: newVisibility,
+            oldVisibility, oldVisibility
+        })
     })
 }
